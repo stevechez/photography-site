2bd98f08e029fc2f10188c2c62e12b6da71968af
Steve M <stevechez@gmail.com>
2023-04-30 07:09:02 -0700

    first commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+export default function MyApp({ children }: Props) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
